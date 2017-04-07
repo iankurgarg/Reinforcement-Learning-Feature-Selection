ea5f2715ffbbba1c6ebe22bd162d4af96e59a687
shahanesanket <svshahan@ncsu.edu>
2017-04-07 11:32:09 -0400

    resolving conflicts

--- feature_selection_nn.py
+++ feature_selection_nn.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-original_data = pd.read_csv('data/MDP_Original_data2.csv')
+original_data = pd.read_csv('MDP_Original_data2.csv')
 vec = list(original_data.columns)
 original_data = original_data[vec[6:]]
 length = float(len(original_data))
@@ -26,9 +26,9 @@ def neural_network_model(data):
 	hidden_layer2 = { 'weights' : tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])), 'biases' : tf.Variable(tf.random_normal([n_nodes_hl2])) }
 	output_layer = { 'weights' : tf.Variable(tf.random_normal([n_nodes_hl2,n_nodes_output])), 'biases' : tf.Variable(tf.random_normal([n_nodes_output])) }
 	l1 = tf.add( tf.matmul(data,hidden_layer1['weights'] ) , hidden_layer1['biases'])
-	l1 = tf.nn.relu(l1)
+	l1 = tf.nn.tanh(l1)
 	l2 = tf.add( tf.matmul(l1, hidden_layer2['weights']), hidden_layer2['biases'])
-	l2 = tf.nn.relu(l2)
+	l2 = tf.nn.tanh(l2)
 	output = tf.matmul(l2, output_layer['weights']) + output_layer['biases']
 	return [output,l2]
 
@@ -58,7 +58,7 @@ def neural_network_model(data):
 
 output_df = pd.DataFrame()
 prediction = neural_network_model(x)
-cost = tf.abs(prediction-y)
+cost = tf.abs(prediction[0]-y)
 # cost = tf.reduce_sum( tf.divide( tf.pow((prediction-y), tf.constant(2.0)), tf.constant(length)) )
 optimizer = tf.train.AdamOptimizer(learning_rate = 0.01).minimize(cost)
 saver = tf.train.Saver()
